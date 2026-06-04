788d8128c17e4324b07e1458cf4bc8d0ee4e8f4e
Max Baker <crimsonender99@gmail.com>
2026-06-03 22:18:11 -0700

    bulk of the code done, time to debug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from google.cloud import datastore
+from google.cloud import datastore, storage
+import uuid
 import os
 
 import requests
@@ -12,6 +13,7 @@ from authlib.integrations.flask_client import OAuth
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
 client = datastore.Client()
+BUCKET_NAME = "PLACEHOLDER"
 
 
 #fetch the three environment variables
@@ -27,9 +29,15 @@ USERS= "users"
 
 #error messages
 ERR_BUS_NOT_FOUND = ({"Error": "No business with this business_id exists"}, 403)
+ERR_COURSE_NOT_FOUND = ({"Error": "No course with this course_id exists"},404)
 ERR_MISSING_ATTRS = ({'Error':'The request body is missing at least one of the required attributes'},400)
 ERR_FORBIDDEN = ({'Error':"You are not authorized to access this resource"},403)
 ERR_NOT_JSON= ({'Error':'"Bad request: Body must be JSON"'},400)
+ERR_BAD_METHOD = ({"Error":"Method not recognized"},405)
+ERR_BAD_INSTRUCTOR_ID = ({"Error": "The value of instructor_id is invalid"},409)
+ERR_UNAUTHORIZED = ({"Error":"Invalid jwt!"},401)
+ERR_USER_NOT_FOUND=({"Error":"No user with this id exists"},404)
+ERR_MISSING_FILE = ({"Error":"The request body is missing the file key"},400)
 
 oauth = OAuth(app)
 
@@ -183,24 +191,141 @@ def get_all_users():
     
 
 #get a user. works either as an admin, with an admin token, or if you have the jwt that matches the user
-@app.route('/'+USERS+ "/<int:id>", methods=["GET"])
+@app.route('/' + USERS + "/<int:id>", methods=["GET"])
 def get_user(id):
-    pass
+    try:
+        payload = verify_jwt(request)
+    except AuthError as e:
+        return jsonify(e.error), e.status_code
+
+    sub = payload['sub']  # you were missing this line
+
+    # get the requesting user
+    query = client.query(kind=USERS)
+    query.add_filter('sub', '=', sub)
+    query_results = list(query.fetch())
+    is_admin = bool(query_results) and query_results[0].get('role') == 'admin'  # fixed logic bug
+
+    # get the target user being requested
+    user_key = client.key(USERS, id)
+    user = client.get(key=user_key)
+
+    if user is None:
+        ERR_USER_NOT_FOUND
+
+    # allow if admin OR if the jwt belongs to the requested user
+    is_own_account = user.get('sub') == sub
+
+    if not is_admin and not is_own_account:
+        return jsonify(ERR_FORBIDDEN), 403
+
+    result = dict(user)
+    result['id'] = id
+    result['self'] = request.host_url + USERS + '/' + str(id)
+    return jsonify(result), 200
+
+
 
 #create or update a user's avatar, requires matching jwt
-@app.route("/"+USERS+"<int:id>/avatar")
+@app.route("/"+USERS+"<int:id>/avatar", methods=['POST'])
 def post_user_avatar(id):
-    pass
+    #check if the file is there
+    if 'file' not in request.files:
+        return ERR_MISSING_FILE
+    
+    try:
+        payload = verify_jwt(request)
+    except AuthError as e:
+        return jsonify(e.error), e.status_code
+    
+    sub = payload['sub']
+
+    user_key = client.key(USERS,id)
+    user = client.get(key=user_key)
+
+    if user is None or user.get('sub')!=sub:
+        return ERR_FORBIDDEN
+    
+    #upload files
+    file = request.files['file']
+    random_filename = str(uuid.uuid4()) + '.png'
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+
+    #delete old avatar if one exists
+    if user.get('avatar_filename'):
+        old_blob = bucket.blob(user['avatar_filename'])
+        if old_blob.exists():
+            old_blob.delete()
+
+    #upload the new avatar
+    blob = bucket.blob(random_filename)
+    blob.upload_from_file(file, content_type = 'image/png')
+
+    user['avatar_filename'] = random_filename
+    client.put(user)
+
+    #return the avatar url
+
+    avatar_url = request.host_url + USERS + '/' + str(id) + '/avatar'
+    return jsonify({'avatar_url':avatar_url}) ,200
+
+
 
 #get a user's avatar. requires matching jwt
 @app.route('/'+USERS+'<int:id>/avatar',methods=['GET'])
 def get_user_avatar(id):
-    pass
+    #check the user's jwt
+    try:
+        payload = verify_jwt(request)
+    except AuthError as e:
+        return jsonify(e.error), e.status_code
+    
+    sub = payload['sub']
+
+    user_key = client.key(USERS,id)
+    user = client.get(key=user_key)
+
+    if user is None or user.get('sub') != sub:
+        return ERR_FORBIDDEN
+    if not user.get('avatar_filename'):
+        return jsonify({"Error":"No avatar found"}),404
+    
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(user['avatar_filename'])
+
+    file_data = blob.download_as_bytes()
+    return file_data, 200, {'Content-Type':'image/png'}
 
 #delete a user's avatar, requires matching jwt
 @app.route('/'+USERS+"/<int:id>/avatar",methods=['DELETE'])
 def delete_user_avatar(id):
-    pass
+    #check the user's jwt
+    try:
+        payload = verify_jwt(request)
+    except AuthError as e:
+        return jsonify(e.error), e.status_code
+    
+    sub = payload['sub']
+
+    user_key = client.key(USERS,id)
+    user = client.get(key=user_key)
+
+    if user is None or user.get('sub') != sub:
+        return ERR_FORBIDDEN
+    if not user.get('avatar_filename'):
+        return jsonify({"Error":"No avatar found"}),404
+    
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(user['avatar_filename'])
+    blob.delete()
+
+    del user['avatar_filename']
+    client.put(user)
+
+    return '',204
 
 """
 course schema = 
@@ -222,7 +347,7 @@ def create_course():
             return ERR_NOT_JSON
         
         required_fields = [
-            'subject','number','title','term'
+            'subject','number','title','term','instructor_id'
         ]
 
         missing_fields = [
@@ -244,8 +369,25 @@ def create_course():
         if not query_results or query_results[0].get('role') != "admin":
             return ERR_FORBIDDEN
         
+        new_course = datastore.entity.Entity(key=client.key(COURSES))
+        new_course.update({
+            "subject":content['subject'],
+            'number':content['number'],
+            'title':content['title'],
+            'term':content['term'],
+            'instructor_id':content['instructor_id'],
+            'enrollment':content['enrollment']
+        })
+        
+        client.put(new_course)
+        result = dict(new_course)
+        result['id'] = new_course.key.id
+        result['self'] = request.host_url + COURSES + '/' + str(new_course.key.id)
+
+        return(jsonify(result),201)
+
     else:
-        return jsonify(error="Method not recognized")
+        return ERR_BAD_METHOD
         
 
 
@@ -254,33 +396,205 @@ def create_course():
 #does not return enrollment data
 @app.route('/'+COURSES, methods=['GET'])
 def get_courses():
-    return
+    #try to get jwt but don't require it
+    if request.method == 'GET':
+
+        query = client.query(kind=COURSES)
+        query_results = list(query.fetch())
+        courses = []
+        for course in query_results:
+            c = dict(course)
+            c['id'] =course.key.id
+            c['self'] - request.host_url + COURSES + '/' + str(course.key.id)
+            c.pop('enrollment',None)
+            courses.append(c)
+
+        return jsonify(courses),200
 
 #get information about a single course, unprotected
 #does not return enrollment data
 @app.route("/"+COURSES+"<int:id>",methods=['GET'])
 def get_single_course(id):
-    return
+    #try to get jwt but don't require it
+
+    if request.method == 'GET':
+
+        course_key  = client.key(COURSES,id)
+        course = client.get(key=course_key)
+
+        #check if the course is existent
+        if course is None:
+            ERR_COURSE_NOT_FOUND
+        else:
+            #if the course is properly retrieved, return it
+
+            result = dict(course)
+            result['id'] = id
+            result['self'] = request.host_url + COURSES + '/' + str(id)
+            return(jsonify(result),200)
 
 #update course data, requires admin login
 @app.route('/'+COURSES+'<int:id>',methods=['PATCH'])
 def update_course_data(id):
-    return
+        
+    if request.method == "PATCH":
+        try:
+
+            payload = verify_jwt(request)
+        except Exception:
+            return ERR_FORBIDDEN
+        
+        sub = payload['sub']
+
+        #check if the course exists
+        course_key = client.key(COURSES,id)
+        course = client.get(course_key)
+
+        #check if the user is an admin
+        query = client.query(kind=USERS)
+        query.add_filter('sub','=',sub)
+        query_results = list(query.fetch())
+        is_admin = bool(query_results) and query_results[0].get('role') == 'admin'
+
+        #if not admin, throw a forbidden error
+        if not course or not is_admin:
+            return ERR_FORBIDDEN
+        
+        content=request.get_json()
+        if not content:
+            #empty body is valid, that means there is nothing to be validated
+            c = dict(course)
+            c['id'] = course.key.id
+            c['self'] = request.host_url + COURSES + '/' + str(id)
+            return jsonify(c),200
+        
+    #validate instructor_id if provided (409)
+    if 'instructor_id' in content:
+        instructor_key = client.key(USERS, content['instructor_id'])
+        instructor = client.get(instructor_key)
+        if not instructor or instructor.get('role') != 'instructor':
+            return jsonify(ERR_BAD_INSTRUCTOR_ID),409
+        
+    allowed_fields = {'subject','number','title','term','instructor_id'}
+    for field in allowed_fields:
+        if field in content:
+            course[field] = content[field]
+    
+    client.put(course)
+
+    #convert to dict and return response
+    course_dict = dict(course)
+    course_dict['id'] = course.key.id
+    course_dict['self'] = request.host_url + COURSES + '/' + str(id)
+    
+    return jsonify(course_dict),200
 
 #delete a course, requires admin login
 @app.route('/'+COURSES+'<int:id>',methods=['DELETE'])
 def delete_course(id):
-    return
+    if request.method == "DELETE":
+        payload = verify_jwt(request)
+        sub = payload['sub']
+
+        #check if the course exists
+        course_key = client.key(COURSES,id)
+        course = client.get(course_key)
+
+        #check if the user is an admin
+        query = client.query(kind=USERS)
+        query.add_filter('sub','=',sub)
+        query_results = list(query.fetch())
+        is_admin = bool(query_results) and query_results[0].get('role') == 'admin'
+
+        #if the course does not
+        if course is None:
+            return ERR_COURSE_NOT_FOUND
+        
+        else:        
+            #if the course exists, check if the user is either an admin or the cours owner
+            if not is_admin:
+                return ERR_FORBIDDEN
+            else:
+                client.delete(course_key)
+                return('',204)
 
 #update enrollment of a course. Requires either a admin login, or for your jwt to correspond to the course owner.
 app.route('/'+COURSES+"<int:id>/students",methods=['PATCH'])
 def update_course_enrollment(id):
-    return
+    if request.method == "PATCH":
+        payload = verify_jwt(request)
+        sub = payload['sub']
+
+        #check if the course exists
+        course_key = client.key(COURSES,id)
+        course = client.get(course_key)
+
+        if course is None:
+            return ERR_COURSE_NOT_FOUND
+        
+        else:
+
+            #check iof the user is admin
+            query = client.query(kind=USERS)
+            query.add_filter('sub','=',sub)
+            query_results = list(query.fetch())
+            is_admin = bool(query_results) and query_results[0].get('role') == 'admin'
+
+            #check if the user is either an admin or the owner of the course
+            if not is_admin or (course['instructor_id']!=sub):
+                return ERR_FORBIDDEN
+            else:
+                content = request.get_json()
+                
+                #if there is no content, just return the course as is
+                if not content:
+                    #empty body is valid, that means there is nothing to be validated
+                    c = dict(course)
+                    c['id'] = course.key.id
+                    c['self'] = request.host_url + COURSES + '/' + str(id)
+                    return jsonify(c),200
+                
+
+                course['enrollment'] = content['enrollment']
+                client.put(course)
+
+                #convert to dict and return response
+                c = dict(course)
+                c['id'] = course.key.id
+                c['self'] = request.host_url + COURSES + '/' + str(id)
+                
+                return jsonify(c),200
 
 #get enrollment of a course. Requires either an admin login, or for the jwt to match that of the course owner
 app.route('/'+COURSES+"<int:id>/students",methods=['GET'])
 def get_course_enrollment(id):
-    return
+    if request.method == "GET":
+        try:
+            payload = verify_jwt(request)
+        except Exception:
+            return ERR_UNAUTHORIZED
+        
+        sub = payload['sub']
+
+        course_key = client.key(COURSES,id)
+        course = client.get(course_key)
+
+        if course is None:
+            return ERR_COURSE_NOT_FOUND
+
+        #check if the user is an admin
+        query = client.query(kind=USERS)
+        query.add_filter('sub','=',sub)
+        query_results = list(query.fetch())
+        is_admin = bool(query_results) and query_results[0].get('role') == 'admin'
+
+        #check if the user is an admin 
+        #check if the user is either an admin or the owner of the course
+        if not is_admin or (course['instructor_id']!=sub):
+            return ERR_FORBIDDEN
+        
+        enrollment = course.get('enrollment',[])
+        return jsonify(enrollment),200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
--- populate.py
+++ populate.py
@@ -1,26 +1,28 @@
 from google.cloud import datastore
-USERS= "users"
+
+USERS = "users"
+
+client = datastore.Client()  # move this to the top, outside the function
 
 def populate_users():
-    users=[
-        {'sub':'auth0|6a0e11ae5ca6737d2af5406b', 'role':'admin'},
-        {'sub':'auth0|6a0e11bebf725ad312e51dda', 'role':'instructor'},
-        {'sub':'auth0|6a0e11c75ca6737d2af5408a', 'role':'instructor'},
-        {'sub':'auth0|6a0e11d2cfd8838127ad6546', 'role':'student'},
-        {'sub':'auth0|6a0e11df5ca6737d2af540a3', 'role':'student'},
-        {'sub':'auth0|6a0e12025ca6737d2af540cb', 'role':'student'},
-        {'sub':'auth0|6a0e12105ca6737d2af540dc', 'role':'student'},
-        {'sub':'auth0|6a0e12200580365abf1fb3e6', 'role':'student'},
-        {'sub':'auth0|6a0e122c0580365abf1fb3fa', 'role':'student'},
+    users = [
+        {'sub': 'auth0|6a0e11ae5ca6737d2af5406b', 'role': 'admin'},
+        {'sub': 'auth0|6a0e11bebf725ad312e51dda', 'role': 'instructor'},
+        {'sub': 'auth0|6a0e11c75ca6737d2af5408a', 'role': 'instructor'},
+        {'sub': 'auth0|6a0e11d2cfd8838127ad6546', 'role': 'student'},
+        {'sub': 'auth0|6a0e11df5ca6737d2af540a3', 'role': 'student'},
+        {'sub': 'auth0|6a0e12025ca6737d2af540cb', 'role': 'student'},
+        {'sub': 'auth0|6a0e12105ca6737d2af540dc', 'role': 'student'},
+        {'sub': 'auth0|6a0e12200580365abf1fb3e6', 'role': 'student'},
+        {'sub': 'auth0|6a0e122c0580365abf1fb3fa', 'role': 'student'},
     ]
-
     for user in users:
         key = client.key(USERS)
         entity = datastore.Entity(key=key)
         entity.update(user)
         client.put(entity)
+        print(f"Added user {user['sub']} with key ID: {entity.key.id}")  # helpful to see the generated IDs
 
 if __name__ == '__main__':
-    client = datastore.Client()
     print("populating database")
     populate_users()
